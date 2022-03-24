7e13bec45fb79e7d0cdc18b4033e66abba2ee501
div <div.pat499@gmail.com>
2022-03-24 09:11:12 -0400

    Added the crawl functionality

--- main.go
+++ main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
+	"strings"
+
+	"golang.org/x/net/html"
 )
+
+func getHref(t html.Token) (ok bool, href string) {
+	for _, a := range t.Attr {
+		if a.Key == "href" {
+			href = a.Val
+			ok = true
+		}
+
+	}
+	return
+}
 
 func main() {
 
@@ -27,9 +42,62 @@ func main() {
 		}
 	}
 
-	fmt.Println("\n\nFound ", len(foundUrls), " urls: \n\n")
+	fmt.Println("\n\nFound ", len(foundUrls), " urls: ")
 
 	for url, _ := range foundUrls {
 		fmt.Println("\t" + url)
 	}
+	fmt.Print("\n\n")
+}
+
+func crawl(url string, ch chan string, chFin chan bool) {
+	res, err := http.Get(url)
+
+	defer func() {
+		chFin <- true
+	}()
+
+	if err != nil {
+		fmt.Print("Error: ", err, "\n For URL\t", url)
+		return
+	}
+
+	body := res.Body
+
+	defer body.Close()
+
+	z := html.NewTokenizer(body)
+
+	for {
+		curToken := z.Next()
+
+		switch {
+
+		case curToken == html.ErrorToken:
+			return
+
+		case curToken == html.StartTagToken:
+			ancTag := z.Token()
+
+			isAnchor := ancTag.Data == "a"
+
+			if !isAnchor {
+				continue
+			}
+
+			ok, url := getHref(ancTag)
+
+			if !ok {
+				continue
+			}
+
+			hasProto := strings.Index(url, "http") == 0
+
+			if hasProto {
+				ch <- url
+			}
+		}
+
+	}
+
 }
